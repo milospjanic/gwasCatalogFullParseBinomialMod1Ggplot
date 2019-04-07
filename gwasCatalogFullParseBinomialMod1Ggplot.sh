a76513b660d31e513fece363a03f5be9b1c5469a
Milos Pjanic <pjanic.milos@gmail.com>
2019-04-06 17:02:10 -0700

    Update gwasCatalogFullParseBinomialMod1Ggplot.sh

--- gwasCatalogFullParseBinomialMod1Ggplot.sh
+++ gwasCatalogFullParseBinomialMod1Ggplot.sh
@@ -220,9 +220,9 @@ echo "print(\"***Final table - top 25 categories - for plot - output.pdf***\")" 
 echo "data">>script.R
 
 #removing Inf > to 10x max value
-echo "data[data\$LogP == \"Inf\",]\$LogP<-0">>script.R
-echo "data[data\$LogP == \"0\",]\$LogP<-max(data\$LogP)*10">>script.R
-echo "data">>script.R
+#echo "data[data\$LogP == \"Inf\",]\$LogP<-0">>script.R
+#echo "data[data\$LogP == \"0\",]\$LogP<-max(data\$LogP)*10">>script.R
+#echo "data">>script.R
 
 #making ggplot2 graph
 echo "library(ggplot2)" >> script.R
